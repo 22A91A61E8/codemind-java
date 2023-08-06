88963515ac5c2be9ad6890b26c6316a5ebe34b4f
Eeli Divya <119840639+22A91A61E8@users.noreply.github.com>
2023-08-06 19:59:48 +0530

    import java.util.Scanner; public class main{     public static void main(String[] args)     {         int h;         Scanner sc=new Scanner(System.in);         h=sc.nextInt();         double a;         a=h*2.54;         System.out.printf("%.2f",a);
        }
    }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args)
+    {
+        int h;
+        Scanner sc=new Scanner(System.in);
+        h=sc.nextInt();
+        double a;
+        a=h*2.54;
+        System.out.printf("%.2f",a);
+        
+    }
+}
