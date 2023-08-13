d69c9d119d71e5d1237463b38e66cf419634c349
Eeli Divya <119840639+22A91A61E8@users.noreply.github.com>
2023-08-13 22:41:19 +0530

    import java.util.Scanner; public class Ultimate{     public static void main(String[] args)     {         float r;         Scanner sc=new Scanner(System.in);         r=sc.nextFloat();         double a;         a=3.14*r*r;         System.out.printf("%.2f",a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ultimate{
+    public static void main(String[] args)
+    {
+        float r;
+        Scanner sc=new Scanner(System.in);
+        r=sc.nextFloat();
+        double a;
+        a=3.14*r*r;
+        System.out.printf("%.2f",a);
+    }
+}
