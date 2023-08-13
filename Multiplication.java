c86cf1266efd22e267b20dcfa5b352b521fdfe79
Eeli Divya <119840639+22A91A61E8@users.noreply.github.com>
2023-08-13 23:28:55 +0530

    import java.util.Scanner; public class Ultimate{     public static void main(String[] args)     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         int k;         k=a*b;         System.out.println(k);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ultimate{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int k;
+        k=a*b;
+        System.out.println(k);
+    }
+}
