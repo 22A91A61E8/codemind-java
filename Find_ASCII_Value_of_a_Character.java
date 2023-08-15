778bcd3f0f2983aa56ec3d7b07049a8a6607e418
Eeli Divya <119840639+22A91A61E8@users.noreply.github.com>
2023-08-15 21:19:15 +0530

    import java.util.Scanner; public class ultimate{     public static void main(String[] args)     {         char a;         Scanner sc=new Scanner(System.in);         a=sc.next().charAt(0);         System.out.print((int)a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ultimate{
+    public static void main(String[] args)
+    {
+        char a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.next().charAt(0);
+        System.out.print((int)a);
+    }
+}
