ffb0a4277edaf917c910c2853537f5902e22a5da
Eeli Divya <119840639+22A91A61E8@users.noreply.github.com>
2023-08-20 09:22:37 +0530

    import java.util.Scanner; public class Ultimate{     public static void main(String[] args)     {         int n,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         for(i=1;i<=10;i++)         {             System.out.print(i+" ");         }     } }

---
+++ 1_to_10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ultimate{
+    public static void main(String[] args)
+    {
+        int n,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=1;i<=10;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
