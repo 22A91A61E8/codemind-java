402b6bb50d2b7b054ad889475c1e86f808629cd3
Eeli Divya <119840639+22A91A61E8@users.noreply.github.com>
2023-08-20 13:05:18 +0530

    import java.util.Scanner; public class ultimate{     public static void main(String[] args)     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         for(int i=a;i<=b;i++)         {             if(i%2==0)             {                 System.out.print(i+" ");             }         }     } }

---
+++ Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ultimate{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(int i=a;i<=b;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
