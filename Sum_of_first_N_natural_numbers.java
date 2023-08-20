462aeeedd48260bd3922fed29f8ebfdba006a63c
Eeli Divya <119840639+22A91A61E8@users.noreply.github.com>
2023-08-20 10:43:42 +0530

    import java.util.Scanner; public class Ultimate{     public static void main(String[] args)     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int s=0;         for(int i=1;i<=n;i++)         {           s=s+i;         }         System.out.printf("%d",s);     } }

--- Sum_of_first_N_natural_numbers.java
+++ Sum_of_first_N_natural_numbers.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class ultimate
-{
+public class Ultimate{
     public static void main(String[] args)
     {
-        int i,n,s=0;
+        int n;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
-        for(i=1;i<=n;i++)
+        int s=0;
+        for(int i=1;i<=n;i++)
         {
-            s=s+i;
+          s=s+i;
         }
         System.out.printf("%d",s);
     }
